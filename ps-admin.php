772b230cc5131fcb45232a7946d3558b4ce8893e
ilivanoff <ivanov.ilya.alex@gmail.com>
2016-02-15 22:25:07 +0300

    www

---
+++ ps-admin.php
@@ -0,0 +1,7 @@
+<?php
+
+//$LOGGING_ENABLED = false;
+
+require_once 'ps-includes/MainImportAdmin.php';
+WebPages::buildCurrent();
+?>
---
+++ ps-ajax.php
@@ -0,0 +1,5 @@
+<?php
+
+//Просто вызываем скрипт, который и сделает всю работу
+require_once __DIR__ . '/ps-includes/ajax/Action.php';
+?>
